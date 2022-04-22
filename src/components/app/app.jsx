0ac8990102446497be2f29abac3254b9547399fe
unknown <tcherepanov.kir@gmail.com>
2022-04-22 17:46:19 +0300

    feat: Сверстать компонент Main

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,12 @@
 import AppHeader from '../app-header/app-header';
+import Main from '../main/main';
 import styles from './app.module.css';
 
 const App = () => {
   return (
     <div className={styles.app}>
       <AppHeader />
+      <Main />
     </div>
   );
 }
