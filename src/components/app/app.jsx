71f1e4c114b497a31ad81bdb249576564d1bd473
unknown <tcherepanov.kir@gmail.com>
2022-04-22 17:24:56 +0300

    feat: Сверстать компонент AppHeader

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import AppHeader from '../app-header/app-header';
+import styles from './app.module.css';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
