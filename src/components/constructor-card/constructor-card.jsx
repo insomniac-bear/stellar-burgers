cc62cab0dd899206d7f886879471b576b0c719fa
unknown <tcherepanov.kir@gmail.com>
2022-04-26 01:12:25 +0300

    feat: Подготовить компоненты конструктора бургеров

---
+++ src/components/constructor-card/constructor-card.jsx
@@ -0,0 +1,14 @@
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import cardStyles from './constructor-card.module.css';
+
+const ConstructorCard = (props) => {
+  return (
+    <article className={cardStyles.card}>
+      <img className='pl-4 pr-4' src={props.image} alt={props.name} />
+      <p className={`${cardStyles.price} text text_type_digits-default mt-1 mb-1`}>{props.price} <CurrencyIcon /></p>
+      <h3 className={`${cardStyles.title} text text_type_main-default`}>{props.name}</h3>
+    </article>
+  );
+}
+
+export default ConstructorCard;
---
+++ src/components/ingridients-list/ingridients-list.jsx
@@ -0,0 +1,20 @@
+import ConstructorCard from "../constructor-card/constructor-card";
+import Title from "../title/title";
+import listStyles from './ingridients-list.module.css';
+
+const IngridientsList = ({ itemList, itemType }) => {
+  return(
+    <div className={`${listStyles.container} mb-10`}>
+      <Title tag={'h2'} className='mb-6'>{itemType.NAME}</Title>
+      <ul className={`${listStyles.list} pr-4 pl-4`}>
+        {itemList.map((item, index) =>
+          <li key={index}>
+            <ConstructorCard image={item.image} name={item.name} price={item.price} />
+          </li>)
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default IngridientsList
