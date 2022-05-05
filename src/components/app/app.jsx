4f8bddfd720664288b0943dff1097ff6a7a8890e
unknown <tcherepanov.kir@gmail.com>
2022-05-05 21:13:53 +0300

    feat: Добавить метод загрузки данных с сервера

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,36 @@
+import { useState, useEffect } from 'react';
 import AppHeader from '../app-header/app-header';
 import Main from '../main/main';
+import { BASE_URL } from '../../utils/const';
 import styles from './app.module.css';
 
 const App = () => {
+  const [ingridients, setIngridients] = useState({
+    isLoading: false,
+    hasError: false,
+    errorMessage: '',
+    data: []
+  });
+
+  useEffect(() => {
+    getIngridients();
+  }, []);
+
+  const getIngridients = () => {
+    setIngridients({ ...ingridients, hasError: false, isLoading: true });
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(res => setIngridients({ ...ingridients, data: res.data, isLoading: false }))
+      .catch(err => {
+        setIngridients({ ...ingridients, isLoading: false, hasError: true, errorMessage: err.message })
+      });
+  };
+
+
   return (
     <div className={styles.app}>
       <AppHeader />
-      <Main />
+      { !ingridients.isLoading && <Main ingridients={ingridients.data}/> }
     </div>
   );
 }
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,15 +1,20 @@
+import PropTypes from 'prop-types';
+import { ingridientDataTypes } from '../../utils/const';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import BurgerIngridients from '../burger-ingredients/burger-ingridients';
 import mainStyles from './main.module.css';
-import { data } from '../../utils/data'
 import { order } from '../../utils/order';
-const Main = () => {
+const Main = ({ ingridients }) => {
   return (
     <main className={mainStyles.main}>
-      <BurgerIngridients data={data}/>
+      <BurgerIngridients data={ingridients}/>
       <BurgerConstructor order={order} />
     </main>
   );
+}
+
+Main.propTypes = {
+  ingridients: PropTypes.arrayOf(ingridientDataTypes.isRequired).isRequired
 }
 
 export default Main;
