213be9c7aabd6c88b7f59723398dd24dce7cbaaa
unknown <tcherepanov.kir@gmail.com>
2022-05-07 01:46:35 +0300

    feat: Добавить модальные окна

---
+++ src/components/ingridient-details/ingridient-details.jsx
@@ -0,0 +1,35 @@
+import { ingridientDataTypes } from '../../utils/const';
+import detailsStyles from './ingridient-details.module.css';
+
+const IngridientDetails = ({ingridient}) => {
+  return(
+    <div className={detailsStyles.container}>
+      <img src={ingridient.image_large} className={'mb-4'} alt={ingridient.name} />
+      <p className={`${detailsStyles.name} text text_type_main-medium mb-8`}>{ingridient.name}</p>
+      <ul className={detailsStyles.list}>
+        <li className={detailsStyles.item}>
+          <p className='text text_type_main-default mb-2'>Калории, ккал</p>
+          <p className='text text_type_digits-medium'>{ingridient.calories}</p>
+        </li>
+        <li className={detailsStyles.item}>
+          <p className='text text_type_main-default mb-2'>Белки, г</p>
+          <p className='text text_type_digits-medium'>{ingridient.proteins}</p>
+        </li>
+        <li className={detailsStyles.item}>
+          <p className='text text_type_main-default mb-2'>Жиры, г</p>
+          <p className='text text_type_digits-medium'>{ingridient.fat}</p>
+        </li>
+        <li className={detailsStyles.item}>
+          <p className='text text_type_main-default mb-2'>Углеводы, г</p>
+          <p className='text text_type_digits-medium'>{ingridient.carbohydrates}</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+IngridientDetails.propTypes = {
+  ingridient: ingridientDataTypes.isRequired
+}
+
+export default IngridientDetails;
---
+++ src/components/order-deatils/order-details.jsx
@@ -0,0 +1,16 @@
+import detailsStyles from './order-details.module.css';
+
+const OrderDetails = () => {
+  return (
+    <div className={detailsStyles.container}>
+      <p className={`${detailsStyles.number} text text_type_digits-large pt-5 mb-8`}>034536</p>
+      <p className={`${detailsStyles.main_text} text text_type_main-medium mb-15`}>идентификатор заказа</p>
+      <div className={`${detailsStyles.decorate} mb-15`}>
+      </div>
+      <p className={`${detailsStyles.main_text} text text_type_main-small mb-2`}>Ваш заказ начали готовить</p>
+      <p className={`${detailsStyles.sub_text} text text_type_main-small mb-15`}>Дождитесь готовности на орбитальной станции</p>
+    </div>
+  );
+}
+
+export default OrderDetails;
